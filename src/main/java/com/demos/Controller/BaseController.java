447f9f6fa1586c9b2946a29dfc27d8045fc8e579
HHH <2824253303@qq.com>
2023-11-25 17:25:05 +0800

    完成session用户数据的保存和登录的控制层和登录页面的使用

--- src/main/java/com/demos/Controller/BaseController.java
+++ src/main/java/com/demos/Controller/BaseController.java
@@ -1,11 +1,15 @@
 package com.demos.Controller;
 
+import com.demos.service.ex.PasswordNotMatchException;
 import com.demos.service.ex.ServiceException;
+import com.demos.service.ex.UserNotFoundException;
 import com.demos.service.ex.UsernameDuplicateException;
 import com.demos.utils.JsonResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//控制层的基类
+import javax.servlet.http.HttpSession;
+
+//控制层 异常的基类
 public class BaseController {
     public static final int OK=200;
     //请求处理方法：这个方法的返回值就是需要传递给前端的数据
@@ -18,10 +22,25 @@ public class BaseController {
         if(e instanceof UsernameDuplicateException){
             result.setState(4000);
             result.setMessage("用户名被占用");
-        }else if(e instanceof ServiceException){
+        }else if(e instanceof UserNotFoundException){
+            result.setState(5001);
+            result.setMessage("用户不存在");
+        }else if(e instanceof PasswordNotMatchException){
+            result.setState(5002);
+            result.setMessage("用户的密码错误异常");
+        }
+        else if(e instanceof ServiceException){
             result.setState(5000);
             result.setMessage("注册时候产生位置的异常");
         }
         return result;
+    }
+
+    //当前用户登录的数据设置，设置到全局的session中
+    protected final Integer getUidFromSession(HttpSession session){
+       return Integer.valueOf(session.getAttribute("uid").toString());
+    }
+    protected final String getUsernameFromSession(HttpSession session){
+        return session.getAttribute("username").toString();
     }
 }
--- src/main/java/com/demos/Controller/UserController.java
+++ src/main/java/com/demos/Controller/UserController.java
@@ -2,12 +2,12 @@ package com.demos.Controller;
 
 import com.demos.entity.User;
 import com.demos.service.IUserService;
-import com.demos.service.ex.InsertException;
-import com.demos.service.ex.UsernameDuplicateException;
 import com.demos.utils.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 //@Controller
 @RestController// == @Controller + @RequestBody 表示此方法的响应结果以json格式进行数据的响应到前端
@@ -33,9 +33,30 @@ public class UserController extends BaseController{
 //        }
 //        return result;
 
+    /*1、接受数据方式：请求处理的方法的参数列表设置为pojo类型来接受前端的数据
+    SpringBoot会将前端的Url地址中的参数名和pojo类的属性名进行比较，如果
+    这两个名称项目，则将值注入到pojo对应属性上
+    * */
     @RequestMapping("reg")
     public JsonResult<Void> reg(User user){
         userService.reg(user);
         return new JsonResult<>(OK);
+    }
+
+    /*2、接受数据方式：请求处理的方法的参数列表设置为非pojo类型来接受前端的数据
+SpringBoot会将前端的Url地址中的参数名和pojo类的属性名进行比较，如果
+名称相同，则自动完成值的依赖注入
+* */
+    @RequestMapping("login")
+    public JsonResult<User> login(String username, String password, HttpSession session){
+        User data = userService.login(username,password);
+        //向session对象中完成数据的绑定（session全局的）
+        session.setAttribute("uid",data.getUid());
+        session.setAttribute("username",data.getUsername());
+
+        //获取session中绑定的数据
+//        System.out.println(getUidFromSession(session));
+//        System.out.println(getUsernameFromSession(session));
+        return new JsonResult<User>(OK,data);
     }
 }
